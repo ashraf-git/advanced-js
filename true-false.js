c2fd9c9b877441b2d807f5cccfcf3cace0ce54fe
ashraf-git <aliashraf.hena@gmail.com>
2020-08-18 13:02:08 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// const name = " ";
+let name =12;
+console.log(name);
+if (name || name == 0) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// falsy::
+// false;
+// 0
+// ""
+// undefined;
+// null
+// Na
+
+// truthy::
+// "0"
+// ' '
+// {}
+// []
+// 'false'
